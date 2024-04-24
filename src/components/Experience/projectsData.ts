e39330ed7589756eaf8e798cab5a6276d4cb2e2b
MonikaHoy <77454575+MonikaHoy@users.noreply.github.com>
2024-04-24 13:08:58 +0200

    update project description

--- src/components/Experience/projectsData.ts
+++ src/components/Experience/projectsData.ts
@@ -2,7 +2,7 @@ type Projects = {
   title: string;
   url?: string;
   description: string;
-  date?: number;
+  date?: number | string;
 };
 
 const projects: Projects[] = [
@@ -10,8 +10,8 @@ const projects: Projects[] = [
     title: "grow",
     url: "https://github.com/MonikaHoy/grow",
     description:
-      "Grow was my first mobile project and what made me want to be a mobile developer. Inspired from tracking my plants' health, I built a plant journal application to track my plants with images. I developed and built the app from scratch in React Native and Expo, using React Navigation and Firebase Cloud Firestore database. I am currently working on moving it from Expo to React Native and refactoring the code to TypeScript.",
-    date: 2020,
+      "Grow was my first mobile project and what made me want to be a mobile developer. Inspired from tracking my plants' health, I built a plant journal application to track my plants with images. I developed and built the app from scratch in React Native and Expo, using React Navigation and Firebase Cloud Firestore database. I recently moved it from Expo to React Native and refactored the code to TypeScript.",
+    date: "2020 & 2024",
   },
 ];
 
